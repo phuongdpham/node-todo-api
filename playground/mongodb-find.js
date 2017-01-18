a757d51b4884c748748d11aa1b66e9e764ce0ede
Phuong D. Pham <Phuong D. Pham>
2017-01-18 21:14:56 +0700

    Init commit

---
+++ playground/mongodb-find.js
@@ -0,0 +1,32 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) {
+    return console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connect to MongoDB server');
+
+  // db.collection('Todos').find({
+  //   _id: new ObjectID('587e4e8aa4fb5a2108b18231')
+  // }).toArray().then((docs) => {
+  //   console.log('Todos');
+  //   console.log(JSON.stringify(docs, undefined, 2));
+  // }, (err) => {
+  //   console.log('Unable to fetch todos', err);
+  // });
+
+  // db.collection('Todos').find().count().then((count) => {
+  //   console.log(`Todos count: ${count}`);
+  // }, (err) => {
+  //   console.log('Unable to fetch todos', err);
+  // });
+
+  db.collection('Users').find({name: 'Phuong'}).toArray().then((docs) => {
+    console.log(JSON.stringify(docs, undefined, 2));
+  }, (err) => {
+
+  });
+
+  // db.close();
+});
