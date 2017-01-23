26ae2c82f8dd8af34bde6ecb938f6c32e0110256
Phuong D. Pham <phamduyphuong10@gmail.com>
2017-01-23 13:21:44 +0700

    Associate users with todos

--- server/tests/seed/seed.js
+++ server/tests/seed/seed.js
@@ -18,16 +18,22 @@ const users = [{
   _id: userTwoId,
   email: 'mai@example.com',
   password: 'userTwoPass',
+  tokens: [{
+    access: 'auth',
+    token: jwt.sign({_id: userTwoId, access: 'auth'}, 'abc123').toString()
+  }]
 }];
 
 const todos = [{
   _id: new ObjectId(),
-  text: 'First test todo'
+  text: 'First test todo',
+  _creator: userOneId
 }, {
   _id: new ObjectId(),
   text: 'Second test todo',
   completed: true,
-  completedAt: 333
+  completedAt: 333,
+  _creator: userTwoId
 }];
 
 const populateUsers = (done) => {
